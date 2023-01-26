a5d58048b2b6f8d90f5ce3a9d90998a11e07013b
SMP_TG <123643433+Frostytechs@users.noreply.github.com>
2023-01-26 20:36:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone  https://github.com/GYPZYTECHS/JENNIE_DUDE
+  git clone  https://github.com/CrazyDeveloperTG/Doctor-Strange.git /Doctor-Strange
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /JENNIE_DUDE
+  git clone $UPSTREAM_REPO /Doctor-Strange
 fi
-cd /JENNIE_DUDE
+cd /Doctor-Strange
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 JENNIE_QUEEN....🍁"
 python3 bot.py
